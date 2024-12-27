ffbee90da2cb87863911099c4a91b067298ba27b
mvenkatarama <mvenkatarama@binghamton.edu>
2024-12-27 01:58:40 -0500

    Added data.js

---
+++ src/data/data.js
@@ -0,0 +1,214 @@
+const data ={
+
+
+    "Home": {
+        "name": "Mukhil Venkataramanan",
+        "description": "Software Engineer",
+    },
+
+
+    "About": {
+        "title": "About me",
+
+        "description_1": "A Computer Science Graduate Student and Teaching/Research Assistant at State University of New York(SUNY) " +
+        "at Binghamton. As a Research Assistant, I'm working on a Machine Learning project, specifically in the domain of Information" +
+        "Retrieval(IR) using the Retrieval Augmented Generation(RAG) Framework. I have also worked on enhancing the Online Preference " +
+        "Reporting and Aggregation(OPRA) website aiding the data collection part related to the research field of Computational Social " +
+        "Choice and Group Decision Making.", 
+ 
+        "description_2": "Being an Ex-Software Engineer, I have worked and developed impactful features on the 'Build & Price' module of Ford cars at " +
+        "Ford Motor Company across six different markets.  I was awarded twice for my professional excellence and commitment.",
+
+        "description_3":"Currently, I'm actively looking for 2025 internship/Co-op/Full-time opportunities. I will be grateful and appreciate your help " +
+         "in any form of networking or a referral which will ease my job search. \n"
+    },
+
+
+    "Experience": {
+        "title": "Experience",
+        "jobs": [
+            {
+                "title": "Teaching and Research Assistant",
+                "company": "SUNY Binghamton",
+                "date": "Aug 2024 - Present",
+                "description": "Worked on building web applications using React and Redux. Collaborated with the design team to implement pixel-perfect designs."
+            },
+            {
+                "title": "Software Engineer",
+                "company": "Ford Motor Company",
+                "date": "April 2023 - Aug 2023",
+                "description": "Developed RESTful APIs using Node and Express. Built frontend applications using React and Redux."
+            },
+            {
+                "title": "Junior Software Engineer",
+                "company": "Ford Motor Company",
+                "date": "Sep 2021 - April 2023",
+                "description": "Developed RESTful APIs using Node and Express. Built frontend applications using React and Redux."
+            }
+        ]
+    },
+
+    "Education": {
+        "title": "Education",
+        "schools": [
+            {
+                "degree": "Master of Science in Computer Science",
+                "school": "SUNY Binghamton",
+                "date": "Aug 2023 - May 2025"
+            },
+            {
+                "degree": "Bachelor of Engineering in Computer Science and Engineering",
+                "school": "Anna University",
+                "date": "2017 - 2021"
+            }
+        ]
+    },
+
+    "Skills": {
+        "title": "Skills",
+        "programming_languages": [
+            {
+                "name": "Python",
+                "level": "90%"
+            },
+            {
+                "name": "Java",
+                "level": "80%"
+            },
+            {
+                "name": "JavaScript",
+                "level": "85%"
+            },
+            {
+                "name": "SQL",
+                "level": "80%"
+            },
+            {
+                "name": "C++",
+                "level": "80%"
+            },
+            {
+                "name": "C",
+                "level": "70%"
+            }
+        ],
+
+        "frame_work_and_tools": [
+            {
+                "name": "React",
+                "level": "90%"
+            },
+            {
+                "name": "Redux",
+                "level": "80%"
+            },
+            {
+                "name": "Django",
+                "level": "85%"
+            },
+            {
+                "name": "Spring Boot",
+                "level": "80%"
+            },
+            {
+                "name": "Git",
+                "level": "80%"
+            },
+            {
+                "name": "Docker",
+                "level": "70%"
+            }
+        ],
+
+        "specialized_tools": [
+            {
+                "name": "AEM",
+                "level": "90%"
+            },
+            {
+                "name": "Akamai",
+                "level": "80%"
+            },
+            {
+                "name": "Dynatrace",
+                "level": "85%"
+            },
+            {
+                "name": "Splunk",
+                "level": "80%"
+            },
+            {
+                "name": "Jira",
+                "level": "80%"
+            },
+            {
+                "name": "SonarQube",
+                "level": "70%"
+            }
+        ],
+
+        // Data Science: Data Collection and Analysis, Deep Neural Networks (Pytorch), CNN, Transformers, Prompt Engineering. 
+        "data_science": [
+            {
+                "name": "Data Collection and Analysis",
+                "level": "90%"
+            },
+            {
+                "name": "Deep Neural Networks (Pytorch)",
+                "level": "80%"
+            },
+            {
+                "name": "CNN",
+                "level": "85%"
+            },
+            {
+                "name": "Transformers",
+                "level": "80%"
+            },
+            {
+                "name": "Prompt Engineering",
+                "level": "80%"
+            }
+        ],
+    },
+
+
+    "Projects": {
+        "title": "Projects",
+        "projects": [
+            {
+                "title": "Next Gen Configurator",
+                "description": "This is a project description",
+                "img": "project2.jpg",
+                "url": "https://example.com"
+            },
+            {
+                "title": "Retrieve relevant articles using RAG",
+                "description": "This is a project description",
+                "img": "project1.jpg",
+                "url": "https://example.com"
+            },
+            {
+                "title": "OPRA",
+                "description": "This is a project description",
+                "img": "project3.jpg",
+                "url": "https://example.com"
+            },
+            {
+                "title": "Interactive Dashboard",
+                "description": "This is a project description",
+                "img": "project3.jpg",
+                "url": "https://example.com"
+            },
+            {
+                "title": "E-Society",
+                "description": "This is a project description",
+                "img": "project3.jpg",
+                "url": "https://example.com"
+            }
+            
+        ]
+    }
+}
+
+export default data;
